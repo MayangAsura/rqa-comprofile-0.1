ee7b2c72981ddd8620455f508cc39634967fa07b
MayangAsura <mayang.asura123@gmail.com>
2026-06-27 00:31:32 +0700

    updated orgzprofile 0.31

--- src/utils/datas/withdraws.js
+++ src/utils/datas/withdraws.js
@@ -212,7 +212,6 @@ export const data = {
 					</>
 
 				)
-                                                                                                                                                                                                                                                                                                      3
 			}
 		}
 
